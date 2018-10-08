5a85674ca52314a11bee9fc43daf1680c740a318
Alan Meile <alan.meile@stud.hslu.ch>
2018-10-08 20:35:17 +0200

    added log4j2

--- experimente/experiment_01/salesmanagement/src/main/java/ch/hslu/wipro/micros/salesmanagement/SalesManagementApp.java
+++ experimente/experiment_01/salesmanagement/src/main/java/ch/hslu/wipro/micros/salesmanagement/SalesManagementApp.java
@@ -1,18 +1,24 @@
 package ch.hslu.wipro.micros.salesmanagement;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class SalesManagementApp {
+    private static final Logger logger = LogManager.getLogger(SalesManagementApp.class);
 
     public static void main(String[] args) {
+
+
         try {
             RabbitMqManager rabbitMqManager = new RabbitMqManager();
             rabbitMqManager.requestArticle(0);
         } catch (IOException e) {
-            System.err.println("cannot connect to rabbitmq!");
+            logger.error("cannot connect to rabbitmq!");
         } catch (TimeoutException e) {
-            System.err.println("a timeout has occurred!");
+            logger.error("a timeout has occurred!");
         }
     }
 }
