ac65d5f6803e6b3b820f7c58dee4d2f71adad0ca
Alan Meile <alan.meile@stud.hslu.ch>
2018-10-08 20:37:01 +0200

    added log4j2

--- experimente/experiment_01/warehousemanagement/src/main/java/ch/hslu/wipro/micros/warehousemanagement/WarehouseManagerApp.java
+++ experimente/experiment_01/warehousemanagement/src/main/java/ch/hslu/wipro/micros/warehousemanagement/WarehouseManagerApp.java
@@ -1,18 +1,23 @@
 package ch.hslu.wipro.micros.warehousemanagement;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class WarehouseManagerApp {
+
+    private static final Logger logger = LogManager.getLogger(WarehouseManagerApp.class);
 
     public static void main(String[] args) {
         try {
             RabbitMqManager rabbitMqManager = new RabbitMqManager();
             rabbitMqManager.listenForArticleRequest();
         } catch (IOException e) {
-            System.err.println("cannot connect to rabbitmq!");
+            logger.error("cannot connect to rabbitmq!");
         } catch (TimeoutException e) {
-            System.err.println("a timeout has occurred!");
+            logger.error("a timeout has occurred!");
         }
     }
 }
