4d8e9b18980668865373b1b02fb71db5f59eca61
Alan Meile <alan.meile@stud.hslu.ch>
2018-10-09 21:22:36 +0200

    user can now input a article request

--- experimente/experiment_01/salesmanagement/src/main/java/ch/hslu/wipro/micros/salesmanagement/SalesManagementApp.java
+++ experimente/experiment_01/salesmanagement/src/main/java/ch/hslu/wipro/micros/salesmanagement/SalesManagementApp.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 public class SalesManagementApp {
@@ -11,15 +12,23 @@ public class SalesManagementApp {
 
     public static void main(String[] args) {
 
-
-        try {
-            RabbitMqManager rabbitMqManager = new RabbitMqManager();
+        try (RabbitMqManager rabbitMqManager = new RabbitMqManager()) {
             rabbitMqManager.sendArticleRequest(0);
             rabbitMqManager.listenForArticleResponse();
+
+            while(true) {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("article id: ");
+                long articleId = sc.nextLong();
+
+                rabbitMqManager.sendArticleRequest(articleId);
+            }
         } catch (IOException e) {
             logger.warn("encountered an IOException while communicating with rabbitmq");
         } catch (TimeoutException e) {
             logger.error("a timeout has occurred!");
+        } catch (NumberFormatException e) {
+            logger.error("input is not valid");
         }
     }
 }
