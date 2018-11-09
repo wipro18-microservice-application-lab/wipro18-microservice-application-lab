7e596de75086149bdc98f206ad0a41acfecd9d4a
Alan Meile <alan.meile@stud.hslu.ch>
2018-11-09 15:11:57 +0100

    order manager sends dummy reply for testing

--- application_lab/salesmanagement/sm-business/src/main/java/ch/hslu/wipro/micros/business/consumer/OrderConsumer.java
+++ application_lab/salesmanagement/sm-business/src/main/java/ch/hslu/wipro/micros/business/consumer/OrderConsumer.java
@@ -1,11 +1,12 @@
 package ch.hslu.wipro.micros.business.consumer;
 
-import com.rabbitmq.client.AMQP;
+import static com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class OrderConsumer extends DefaultConsumer {
 
@@ -15,7 +16,18 @@ public class OrderConsumer extends DefaultConsumer {
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope,
-                               AMQP.BasicProperties properties, byte[] body) throws IOException {
+                               BasicProperties properties, byte[] body) throws IOException {
 
+        String route = "";
+        BasicProperties basicProperties = new BasicProperties
+                .Builder()
+                .correlationId(properties.getCorrelationId())
+                .build();
+
+        super.getChannel().basicPublish(
+                route,
+                properties.getReplyTo(),
+                basicProperties,
+                "ORDER COMPLETED".getBytes(StandardCharsets.UTF_8));
     }
 }
