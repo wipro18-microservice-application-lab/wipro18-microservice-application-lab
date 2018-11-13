ea160f3d89dffce79472e087022ec6ac56ab193d
Alan Meile <alan.meile@stud.hslu.ch>
2018-11-13 09:40:15 +0100

    prepared client for architecture puncture

---
+++ application_lab/client/globals.js
@@ -0,0 +1,4 @@
+var SALES_HEALTH_URL = 'http://localhost:8080/gateway/sales/health';
+
+var WAREHOUSE_HEALTH_URL = 'http://localhost:8080/gateway/warehouse/health';
+var WAREHOUSE_URL = 'http://localhost:8080/gateway/warehouse';
