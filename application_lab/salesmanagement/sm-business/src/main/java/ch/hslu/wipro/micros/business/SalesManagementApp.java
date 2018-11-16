3c593a58f0b6d7e50fd0e12de3c015f9c5f04ea8
Alan Meile <alan.meile@stud.hslu.ch>
2018-11-16 16:47:27 +0100

    retries connecting to rabbit every x sec. for clean docker-compose startup

--- application_lab/salesmanagement/sm-business/src/main/java/ch/hslu/wipro/micros/business/SalesManagementApp.java
+++ application_lab/salesmanagement/sm-business/src/main/java/ch/hslu/wipro/micros/business/SalesManagementApp.java
@@ -7,19 +7,40 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class SalesManagementApp {
     private static final Logger logger = LogManager.getLogger(SalesManagementApp.class);
     private static final ErrorService errorService = new ErrorService();
+    private static ScheduledExecutorService connectionService;
 
     public static void main(String[] args) {
+        Runnable runnable = () -> {
+            try {
+                logger.info("registering sales manager on discovery service");
+                DiscoveryService discoveryService = new DiscoveryService();
+                discoveryService.register();
 
+                startOderManager();
+                connectionService.shutdown();
+            } catch (IOException | TimeoutException e) {
+                errorService.logConnectionError(SalesManagementApp.class);
+            }
+        };
+
+        connectionService = Executors.newSingleThreadScheduledExecutor();
+        connectionService.scheduleAtFixedRate(
+                runnable,
+                10,
+                5,
+                TimeUnit.SECONDS);
+    }
+
+    private static void startOderManager() {
         try {
-            logger.info("registering sales manager on discovery service");
-            DiscoveryService discoveryService = new DiscoveryService();
-            discoveryService.register();
-
             logger.info("order manager handling incoming orders");
             OrderManager orderManager = new OrderManager();
             orderManager.setup().handleIncomingOrders();
