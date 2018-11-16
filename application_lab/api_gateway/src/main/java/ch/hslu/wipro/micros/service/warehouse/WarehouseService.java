9ec26f8875e7f7936f71124210e6f903d5faf0fa
Alan Meile <alan.meile@stud.hslu.ch>
2018-11-16 13:15:24 +0100

    salesmanager runs in docker-compose

--- application_lab/api_gateway/src/main/java/ch/hslu/wipro/micros/service/warehouse/WarehouseService.java
+++ application_lab/api_gateway/src/main/java/ch/hslu/wipro/micros/service/warehouse/WarehouseService.java
@@ -5,10 +5,7 @@ import ch.hslu.wipro.micros.service.sales.CommandFactory;
 import ch.hslu.wipro.micros.service.sales.OrderDTO;
 import ch.hslu.wipro.micros.service.sales.SalesCommand;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -23,16 +20,10 @@ public class WarehouseService {
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createOrder(OrderDTO orderDTO) {
-        SalesCommand command = CommandFactory.createCreateOrderCommand(orderDTO); // Todo Interfaces
+    public Response createOrder(Object object) {
 
-        try {
-            RabbitMqClient client = new RabbitMqClient();
-            client.call(command, "ch.hslu.wipro.micros.Article"); //Todo From discovery
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         return Response.status(201).entity("[{\"id\":\"ec7445cb-44f0-4f6d-95d1-295c9a506f40\",\"name\":\"Samsung Galaxy A6\",\"description\":\"Smartphone\",\"price\":500.0}]\n").build();
     }
 }
