ab60daea994dddc8a524dbd23b4664b5b4fdd485
Alan Meile <alan.meile@stud.hslu.ch>
2018-11-25 14:03:16 +0100

    added repo test -> found and fixed a bug

---
+++ application_lab/salesmanagement/sm-service/src/test/java/ch/hslu/wipro/micros/service/repository/OrderRepositoryServiceTest.java
@@ -0,0 +1,87 @@
+package ch.hslu.wipro.micros.service.repository;
+
+import ch.hslu.wipro.micros.model.order.OrderDto;
+import ch.hslu.wipro.micros.model.order.OrderDtoBuilder;
+import ch.hslu.wipro.micros.model.order.OrderUpdateDto;
+import ch.hslu.wipro.micros.model.order.OrderUpdateResultDto;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class OrderRepositoryServiceTest {
+    private OrderRepositoryService orderRepositoryService;
+    private static int amountOfOrderDto = 0;
+    private long orderDtoId;
+    private OrderDto orderDto;
+
+    @Before
+    public void setUp() throws Exception {
+        Map<Integer, Integer> amountToArticle = new HashMap<>();
+        amountToArticle.put(1, 5);
+        amountToArticle.put(2, 4);
+        amountToArticle.put(3, 1);
+
+        orderDto = new OrderDtoBuilder()
+                .atStatus("Open")
+                .atCustomer(42L)
+                .atTotalPrice(BigDecimal.valueOf(120L))
+                .mapAmountToArticle(amountToArticle)
+                .build();
+
+        orderRepositoryService = new OrderRepositoryService();
+        orderDtoId = orderRepositoryService.setOrder(orderDto);
+        amountOfOrderDto++;
+    }
+
+    @Test
+    public void getOrder() {
+        OrderDto retrievedOrderDto = orderRepositoryService.getOrder(orderDtoId);
+        orderDto.setOrderId(orderDtoId);
+
+        assertEquals(orderDto, retrievedOrderDto);
+    }
+
+    @Test
+    public void setOrder() {
+        OrderDto newOrderDto = new OrderDtoBuilder().build();
+        long newOrderDtoId = orderRepositoryService.setOrder(newOrderDto);
+        newOrderDto.setOrderId(newOrderDtoId);
+
+        OrderDto retrievedOrderDto = orderRepositoryService.getOrder(newOrderDtoId);
+        assertEquals(newOrderDto, retrievedOrderDto);
+    }
+
+    @Test
+    public void getAllOrders() {
+        List<OrderDto> orderDtos = orderRepositoryService.getAllOrders();
+        assertEquals(amountOfOrderDto, orderDtos.size());
+    }
+
+    @Test
+    public void getAllOrdersByCustomerId() {
+        List<OrderDto> orderDtos = orderRepositoryService.getAllOrdersByCustomerId(42L);
+        assertEquals(amountOfOrderDto, orderDtos.size());
+    }
+
+    @Test
+    public void updateStatus() {
+        final String NEW_UPDATE = "New Update";
+
+        OrderUpdateDto orderUpdateDto = new OrderUpdateDto();
+        orderUpdateDto.setStatus(NEW_UPDATE);
+        orderUpdateDto.setOrderId(0);
+
+        OrderUpdateResultDto orderUpdateResultDto = orderRepositoryService.updateStatus(orderUpdateDto);
+        assertEquals("successful", orderUpdateResultDto.getResult());
+
+        OrderDto retrievedOrderDto = orderRepositoryService.getOrder(orderUpdateDto.getOrderId());
+        assertEquals(NEW_UPDATE, retrievedOrderDto.getStatus());
+    }
+}
