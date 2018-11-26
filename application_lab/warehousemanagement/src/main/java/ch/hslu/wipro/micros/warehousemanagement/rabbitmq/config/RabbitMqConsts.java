185c54093c958a4bebe465d00270b81dc020844c
Alan Meile <alan.meile@stud.hslu.ch>
2018-11-26 22:45:13 +0100

    use jar with dependencies

--- application_lab/warehousemanagement/src/main/java/ch/hslu/wipro/micros/warehousemanagement/rabbitmq/config/RabbitMqConsts.java
+++ application_lab/warehousemanagement/src/main/java/ch/hslu/wipro/micros/warehousemanagement/rabbitmq/config/RabbitMqConsts.java
@@ -2,4 +2,7 @@ package ch.hslu.wipro.micros.warehousemanagement.rabbitmq.config;
 
 public class RabbitMqConsts {
     public static final String CONFIG_FILE = "rabbitmq.properties";
+
+    private RabbitMqConsts() {
+    }
 }
