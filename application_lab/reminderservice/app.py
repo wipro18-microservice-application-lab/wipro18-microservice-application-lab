f2c63e3ccfbce5fcb8bd0b80de0c5c82ff20687c
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2018-12-01 14:47:25 +0100

    reminder: prepare json

--- application_lab/reminderservice/app.py
+++ application_lab/reminderservice/app.py
@@ -1,5 +1,7 @@
 import pika
 import time
+import datetime
+import json
 import config
 import rabbit_manager
 
@@ -11,14 +13,38 @@ order_exchange = config.values['order_exchange']
 reminders = []
 
 
+class ReminderEntry:
+    def __init__(self, customer_id, fee, issue_date):
+        self.customer_id = customer_id
+        self.fee = fee
+        self.issue_date = issue_date
+
+    def to_json_str(self):
+        return '{"customerId:"' + self.customer_id \
+               + ',"fee":' + self.fee \
+               + ',"issue_date":' + str(self.issue_date)
+
+
 def listen_for_order_events():
+    """Listen for order complete events.
+    """
     channel = rabbit_manager.prepare_order_channel(callback_order)
     print('[*] Waiting for events...')
     channel.start_consuming()
 
 
+# creates a new timestamp
+def create_timestamp():
+    return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+
+
 def callback_order(ch, method, properties, body):
-    print(body.decode('utf-8', 'ignore'))
+    body_dec = body.decode('utf-8', 'ignore')
+    event = json.loads(body_dec)
+
+    entry = ReminderEntry(event['customerId'], 100, create_timestamp())
+    print(entry.to_json_str())
+    reminders.append(entry)
 
 
 def listen_for_commands():
@@ -40,5 +66,7 @@ def callback_command(ch, method, properties, body):
 
 
 if __name__ == '__main__':
-    Thread(target=listen_for_order_events).start()
-    Thread(target=listen_for_commands).start()
+    #Thread(target=listen_for_order_events).start()
+    #Thread(target=listen_for_commands).start()
+
+    e = ReminderEntry(1,44,create_timestamp())
