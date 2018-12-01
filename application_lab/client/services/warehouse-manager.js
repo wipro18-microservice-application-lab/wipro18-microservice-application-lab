303711338c829ae400325ca1b43756722e1be01c
Alan Meile <alan.meile@stud.hslu.ch>
2018-12-01 10:54:49 +0100

    fixed get article by id

--- application_lab/client/services/warehouse-manager.js
+++ application_lab/client/services/warehouse-manager.js
@@ -31,27 +31,16 @@ function articleGetAllCommand() {
     });
 }
 
-function articleGetByIdCommand(articleIdDto) {
-    $.ajax({
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        type: 'GET',
-        url: WAREHOUSE_URL,
-        data: JSON.stringify(articleIdDto),
-        contentType: "application/json; charset=utf-8",
-        traditional: true,
-        success: function (articleDto) {
-            $('#article-by-id').find('> tbody:last-child')
-                .append(
-                    '<tr>' +
-                    `<td>${articleDto.articleId}</td>` +
-                    `<td>${articleDto.name}</td>` +
-                    `<td>${articleDto.description}</td>` +
-                    `<td>${articleDto.price}</td>` +
-                    `<td>${articleDto.quantity}</td>` +
-                    '</tr>');
-        }
+function articleGetByIdCommand(articleId) {
+    $.get(WAREHOUSE_URL + '/articles/' + articleId, function(articleDto) {
+        $('#article-by-id').find('> tbody:last-child')
+            .append(
+                '<tr>' +
+                `<td>${articleDto.articleId}</td>` +
+                `<td>${articleDto.name}</td>` +
+                `<td>${articleDto.description}</td>` +
+                `<td>${articleDto.price}</td>` +
+                `<td>${articleDto.quantity}</td>` +
+                '</tr>');
     });
 }
