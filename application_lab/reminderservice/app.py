b3c29950b4dd09ab7ae93d26ed8f95e9af4e2105
Cyrill Jauner <cyrill.jauner@stud.hslu.ch>
2018-12-01 14:25:39 +0100

    Auto stash before merge of "master" and "origin/master"

--- application_lab/reminderservice/app.py
+++ application_lab/reminderservice/app.py
@@ -1,39 +1,44 @@
 import pika
+import time
 import config
+import rabbit_manager
+
+from threading import Thread
 
 host = config.values['host']
 order_exchange = config.values['order_exchange']
 
-
-def prepare_channel():
-    """Prepares the channel to the rabbitmq instance
-    :return: channel
-    """
-    # setup connection to the rabbitmq instance
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-    channel = connection.channel()
-
-    # declare a new exclusive queue (rabbitmq declares the name of it)
-    result = channel.queue_declare(exclusive=True)
-    queue_name = result.method.queue
-
-    # binds the queue to the order exchange
-    channel.queue_bind(exchange=order_exchange,
-                       queue=queue_name,
-                       routing_key='order.event.complete')
-    # consumes every message with no ack
-    channel.basic_consume(callback,
-                          queue=queue_name,
-                          no_ack=True)
-    return channel
+reminders = []
 
 
-# callback function to invoke for each incoming message
-def callback(ch, method, properties, body):
-    print(body)
+def listen_for_order_events():
+    channel = rabbit_manager.prepare_order_channel(callback_order)
+    print('[*] Waiting for events...')
+    channel.start_consuming()
+
+
+def callback_order(ch, method, properties, body):
+    print(body.decode('utf-8', 'ignore'))
+
+
+def listen_for_commands():
+    channel = rabbit_manager.prepare_command_channel(callback_command)
+    channel.start_consuming()
+
+
+def callback_command(ch, method, properties, body):
+    all_reminders = reminders
+    reply_properties = pika.BasicProperties(correlation_id=properties.correlation_id)
+
+    ch.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     properties=reply_properties,
+                     body=all_reminders)
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    print("all reminders",all_reminders)
 
 
 if __name__ == '__main__':
-    channel = prepare_channel()
-    print('[*] Waiting for events...')
-    channel.start_consuming()
+    Thread(target=listen_for_order_events).start()
+    Thread(target=listen_for_commands).start()
